493b5b0a2566373a2813a9438c76b181bd2945d6
Michael <abmichael109@gmail.com>
2025-10-20 19:43:06 +0100

    hey

--- api/urls/academic.py
+++ api/urls/academic.py
@@ -21,6 +21,6 @@ router.register(r'grades', GradeViewSet, basename='grade')
 router.register(r'subject-groups', SubjectGroupViewSet, basename='subject-group')
 router.register(r'subjects', SubjectViewSet, basename='subject')
 router.register(r'questions', QuestionViewSet, basename='question')
-
+ 
 urlpatterns = router.urls
 
