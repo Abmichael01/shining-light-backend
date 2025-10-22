df827e763a2246a6388010441dc135a60f53cafe
Michael <abmichael109@gmail.com>
2025-10-22 15:11:18 +0100

    Update migration dependency to reference the previous migration for question model adjustments. This change ensures the migration history remains accurate and relevant following recent updates to the data structure.

--- api/migrations/0011_exam_studentanswer_studentexam_topic_and_more.py
+++ api/migrations/0011_exam_studentanswer_studentexam_topic_and_more.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("api", "0010_alter_student_admission_number"),
+        ("api", "0009_question"),
     ]
 
     operations = [
